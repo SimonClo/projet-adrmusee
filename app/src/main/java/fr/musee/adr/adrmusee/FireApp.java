9934a7f6693f1f02991c102eb35d509179acf3d1
M <m-hammed.bakkouch@student.ecp.fr>
2018-05-24 12:46:34 +0200

    E

---
+++ app/src/main/java/fr/musee/adr/adrmusee/FireApp.java
@@ -0,0 +1,13 @@
+package fr.musee.adr.adrmusee;
+
+import android.app.Application
+import com.firebase.client.Firebase;
+
+
+public class FireApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Firebase.setAndroidContext(this)
+    }
+}
--- app/src/main/java/fr/musee/adr/adrmusee/MainActivity.java
+++ app/src/main/java/fr/musee/adr/adrmusee/MainActivity.java
@@ -2,8 +2,13 @@ package fr.musee.adr.adrmusee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText Email;
+    private Edittext Password;
+    private Button Seconnecter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
